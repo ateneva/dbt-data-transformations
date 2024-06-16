969c62f2f2d30566df78b151a4d4fd0e5c2dfd5d
ateneva <tenevaa21@gmail.com>
2024-06-16 17:28:54 +0200

    CU-86bz7w61w-setup-bq-datasets-needed-for-transformations

---
+++ bq-resources/the-look-datasets.tf
@@ -0,0 +1,35 @@
+resource "google_bigquery_dataset" "the-look-prod" {
+  dataset_id                  = "the_look_prod"
+  friendly_name               = "the-look-prod"
+  description                 = "This dataset has the transformations applied to The Look Ecoomerce"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
+
+resource "google_bigquery_dataset" "the-look-dev" {
+  dataset_id                  = "the_look_dev"
+  friendly_name               = "the-look-dev"
+  description                 = "This dataset tests the transformations applied to The Look Ecoomerce"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
+
+resource "google_bigquery_dataset" "the-look-data-quality" {
+  dataset_id                  = "the_look_data_quality"
+  friendly_name               = "the-look-data-quality"
+  description                 = "This dataset stores the data quality test results of the transformations applied to The Look Ecoomerce"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
