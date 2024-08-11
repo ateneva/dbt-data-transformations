d24cf8095008b6f2d0ca8bc11f00e0e623dd8e5c
ateneva <tenevaa21@gmail.com>
2024-08-11 17:10:27 +0200

    CU-86bz7uraj-datasets-should-be-in-US-because-source-data-is-in-us

--- terraform/the-look-datasets.tf
+++ terraform/the-look-datasets.tf
@@ -2,7 +2,7 @@ resource "google_bigquery_dataset" "the-look-prod" {
   dataset_id                  = "the_look_prod"
   friendly_name               = "the-look-prod"
   description                 = "This dataset has the transformations applied to The Look Ecoomerce"
-  location                    = "EU"
+  location                    = "US"
   default_table_expiration_ms = 3600000
 
   labels = {
@@ -14,7 +14,7 @@ resource "google_bigquery_dataset" "the-look-dev" {
   dataset_id                  = "the_look_dev"
   friendly_name               = "the-look-dev"
   description                 = "This dataset tests the transformations applied to The Look Ecoomerce"
-  location                    = "EU"
+  location                    = "US"
   default_table_expiration_ms = 3600000
 
   labels = {
@@ -26,7 +26,7 @@ resource "google_bigquery_dataset" "the-look-data-quality" {
   dataset_id                  = "the_look_data_quality"
   friendly_name               = "the-look-data-quality"
   description                 = "This dataset stores the data quality test results of the transformations applied to The Look Ecoomerce"
-  location                    = "EU"
+  location                    = "US"
   default_table_expiration_ms = 3600000
 
   labels = {
